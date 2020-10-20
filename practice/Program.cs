677b287dc06838101202676536e22babf1cc1355
karocicavid <69958924+karocicavid@users.noreply.github.com>
2020-10-20 13:23:19 +0400

    Update Program.cs

--- practice/Program.cs
+++ practice/Program.cs
@@ -1,19 +1,20 @@
-﻿using System;
+using System;
 using System.IO;
 
 namespace ConsoleApp
-{   class Index
+{
+    class Index
     {
         Client[] data;
         public Index()
         {
             data = new Client[100];
         }
-        public Client this [int index]
-            {
+        public Client this[int index]
+        {
             set { data[index] = value; }
             get { return data[index]; }
-            }
+        }
     }
     class Client
     {
@@ -25,64 +26,55 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            try
+
+            Index index = new Index();
+            string path = @"D:\test_client";
+            DirectoryInfo dirInfo = new DirectoryInfo(path);
+            if (!dirInfo.Exists)
             {
+                dirInfo.Create();
+            }
+            string filepath = @"D:\test_client\test_client.txt";
+            FileInfo File = new FileInfo(filepath);
+            if (!File.Exists)
+            {
+                File.Create().Close();
+            }
+            using (StreamWriter wr = new StreamWriter(filepath, true, System.Text.Encoding.Default))//writing in file
+            {
+                wr.WriteLine("001; AZE12345678;22\n" +
+                    "002; AZE87652134;50\n" +
+                    "003; AZE87652134;12");
+            }
 
-                Index index = new Index();
-                string path = @"D:\test_client";
-                DirectoryInfo dirInfo = new DirectoryInfo(path);
-                if (!dirInfo.Exists)
-                {
-                    dirInfo.Create();
-                }
-                string filepath = @"D:\test_client\test_client.txt";
-                FileInfo File = new FileInfo(filepath);
-                if (!File.Exists)
-                {
-                    File.Create();
-                }
-                using (StreamWriter wr = new StreamWriter(filepath, true, System.Text.Encoding.Default))//writing in file
-                {
-                    wr.WriteLine("001; AZE12345678;22\n" +
-                        "  002; AZE87652134;50\n" +
-                        "  003; AZE87652134;12");
-                }
-
-                using (StreamReader sr = new StreamReader(filepath))//reading what we write
-                {
-                    for (int i = 1; i <= 3; i++)
-                    {
-                        string str = sr.ReadLine();
-                        string[] line = str.Split(';');
-                        string id = line[0];
-                        string passport = line[1];
-                        string payment = line[2];
-                        index[i] = new Client { id = id, passport = passport, payment = payment };
-
-                    }
-                }
-                int total = 0;
+            using (StreamReader sr = new StreamReader(filepath))//reading what we write
+            {
                 for (int i = 1; i <= 3; i++)
                 {
-                    int payment = Convert.ToInt32(index[i].payment);//стринг конвертируем в инт чтобы сложить
-                    total += payment;
-                }
-                Console.WriteLine(total);
-                string total_string = Convert.ToString(total);// инт конвертируем в стринг
-                string newfilepath = @"D:\test_client\new_test_client.txt";
+                    string str = sr.ReadLine();
+                    string[] line = str.Split(';');
+                    string id = line[0];
+                    string passport = line[1];
+                    string payment = line[2];
+                    index[i] = new Client { id = id, passport = passport, payment = payment };
 
-                using (StreamWriter wr = new StreamWriter(newfilepath, false, System.Text.Encoding.Default))
-                {
-                    wr.WriteLine($"We noticed total sum of payments - {total}");
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine("Files are created,let's check !");
-                    Console.ResetColor();
                 }
             }
-            catch
+            int total = 0;
+            for (int i = 1; i <= 3; i++)
             {
-                Console.ForegroundColor = ConsoleColor.DarkGreen;
-                Console.WriteLine("При втором запуске производится выдача результатов! ");
+                int payment = Convert.ToInt32(index[i].payment);//стринг конвертируем в инт чтобы сложить
+                total += payment;
+            }
+            Console.WriteLine(total);
+            string total_string = Convert.ToString(total);// инт конвертируем в стринг
+            string newfilepath = @"D:\test_client\new_test_client.txt";
+
+            using (StreamWriter wr = new StreamWriter(newfilepath, false, System.Text.Encoding.Default))
+            {
+                wr.WriteLine($"We noticed total sum of payments - {total}");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Files are created,let's check !");
                 Console.ResetColor();
             }
             Console.Read();
